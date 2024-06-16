5ca65bc8821fbcd03d13346489143d6ec8822d0a
ArkadySK <adamkooo2@gmail.com>
2024-06-16 14:03:16 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManialinkToActionscriptAutoUpdater
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            PathTextBox.Text = Properties.Settings.Default.LastSelectedFilePath;
+            ExportFolderTextBox.Text = Properties.Settings.Default.LastExportFolderPath;
+        }
+
+        private void SetStatus(string message, SolidColorBrush brush)
+        {
+            StatusTextBlock.Background = null;
+            var animation = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 1;
+            animation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            StatusTextBlock.Background = brush;
+            StatusTextBlock.BeginAnimation(OpacityProperty, animation);
+            StatusTextBlock.Text = message;
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            string path = PathTextBox.Text;
+            string exportFolder = ExportFolderTextBox.Text;
+            
+            try
+            {
+                ManialinkToActionscriptConverter.Convert(path, exportFolder);
+                SetStatus("The generation was successful!", new SolidColorBrush(Color.FromRgb(150, 250, 150)));
+            }
+            catch (Exception ex)
+            {
+                SetStatus("The generation failed: \n" + ex.Message, new SolidColorBrush(Color.FromRgb(250, 150, 150)));
+            }
+            Properties.Settings.Default.Save();
+        }
+
+        private void SelectFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dg = new()
+            {
+                Filter = "Manialink files (*.xml)|*.xml"
+            };
+            var result = dg.ShowDialog();
+
+            if (result != true)
+                return;
+
+            PathTextBox.Text = dg.FileName;
+            Properties.Settings.Default.LastSelectedFilePath = PathTextBox.Text;
+        }
+
+        private void SelectExportFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolderDialog dg = new();
+            var result = dg.ShowDialog();
+
+            if (result != true)
+                return;
+
+            ExportFolderTextBox.Text = dg.FolderName;
+            Properties.Settings.Default.LastExportFolderPath = ExportFolderTextBox.Text;
+        }
+    }
+}
---
+++ ManialinkToActionscriptConverter.cs
@@ -0,0 +1,37 @@
+﻿
+using System.IO;
+using System.Text;
+
+namespace ManialinkToActionscriptAutoUpdater
+{
+    /// <summary>
+    /// Converts a Manialink file to an Actionscript file. Removes all # symbols and puts the manialink code into a string method.
+    /// </summary>
+    internal class ManialinkToActionscriptConverter
+    {
+        internal static void Convert(string path, string exportFolder)
+        {
+            if (!Directory.Exists(exportFolder))
+                throw new DirectoryNotFoundException("The export folder does not exist.");
+
+            if (!File.Exists(path))
+                throw new FileNotFoundException("The manialink file does not exist.", Path.GetFileName(path));
+
+            string manialinkName = Path.GetFileNameWithoutExtension(path);
+            string exportFileName = Path.Combine(exportFolder, manialinkName + "Manialink.as");
+
+            string manialinkContent = File.ReadAllText(path);
+            manialinkContent = manialinkContent.Replace("#", "\"\"\" + hashtag + \"\"\"");
+
+            StringBuilder sb = new();
+            sb.AppendLine("// Manialink code for " + manialinkName + ".xml");
+            sb.AppendLine("\r\nstring Get"+manialinkName+ "() {\r\n\t");
+            sb.AppendLine("\t//fixing hastag in \"\"\"\r\n\tstring hashtag = \"\\u0023\"; \n");
+            sb.AppendLine("return \"\"\"");
+            sb.Append(manialinkContent);
+            sb.AppendLine("\"\"\";\n}");
+
+            File.WriteAllText(exportFileName, sb.ToString());
+        }
+    }
+}
